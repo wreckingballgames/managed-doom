0034d91747e3a74f2f0e902a5a9b4f52e1ccd313
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-04 07:59:36 -0600

    Restore a line of code I thought was unnecessary
    Experiment concluded. `Skip(1)` is needed in the case that a .WAD file is passed in directly first, and `TakeWhile()` is needed in other cases. I think that's basically correct.
    
    It failed on me for the first time when using `-file` to pass in the .WAD I wanted to play; it was the first argument and the other arguments caused an exception.

--- ManagedDoom/src/CommandLineArgs.cs
+++ ManagedDoom/src/CommandLineArgs.cs
@@ -241,6 +241,7 @@ namespace ManagedDoom
             return args
                 .SkipWhile(arg => arg != name)
                 .Skip(1)
+                .TakeWhile(arg => arg[0] != '-')
                 .ToArray();
         }
 
